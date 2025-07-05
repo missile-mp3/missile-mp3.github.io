2a3332046be32b7030a1aa0f6b1560818533742a
missile.mp3 <balisticalxrocket@gmail.com>
2025-07-05 10:08:23 +0300

    js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const name = "oluh";
+
+setInterval(() => {
+    switch (document.title[0]) {
+        case "|":
+            document.title = `/ ${name}`; break
+        case "/":
+            document.title = `- ${name}`; break
+        case "-":
+            document.title = `\\ ${name}`; break
+        case "\\":
+            document.title = `| ${name}`; break
+    }
+}, 250);
